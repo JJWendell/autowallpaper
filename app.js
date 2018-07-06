542313db3aa1c8b2558bd3d670d2d930ebdcd5f8
JJWendell <33045890+JJWendell@users.noreply.github.com>
2018-07-07 00:41:52 +0300

    comments

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
-/* в папке wallpapers доллжно находиться 24 картинки в формате jpeg;
+//a simple app that autochanges wallpaper every hour 
+
+/* в папке wallpapers должно находиться 24 картинки в формате jpeg;
 формат названия: n.jpeg, n = 1...24; где n - час в который вы хотите поставить
-определенную картинку*/
+определенную картинку
+
+Create 'wallpapers' folder. Add there 24 picture.
+*/
 
 let time = new Date().getHours();
 console.log(time);
@@ -8,9 +13,11 @@ console.log(time);
 const wallpaper = require('wallpaper');
 
 //ставит обои
+//sets wallpaper
 getWallpaper(time);
 
 //функция меняющая обои в зависимости от времени
+//changes a wallpaper depending on the hour  
 function getWallpaper(t) {
     let pic = t + '.jpeg';
 
@@ -24,6 +31,7 @@ function getWallpaper(t) {
 }
 
 //проверяет каждую минуту
+//checks every minute
 setInterval(function() {
         let check = new Date().getHours();
         if (check != time) {
